7396e028d78c9bce0ebd2288615e181955da87d7
Tremaine-Udemy <152989172+Tremaine-Udemy@users.noreply.github.com>
2023-12-08 09:16:44 -0500

    Create SimpleScript.py

---
+++ Exercises/SimpleScript.py
@@ -0,0 +1,14 @@
+# Simple Python Script Exercise
+
+print("Welcome to the Simple Python Script Exercise!")
+
+# Task: Write a function that prints "Hello, [name]!" where [name] is a parameter.
+
+# Example function definition:
+# def greet(name):
+#     print("Hello, " + name + "!")
+
+# Once you've written the function, call it with your name as the argument.
+
+# (Write your function and call it below)
+
